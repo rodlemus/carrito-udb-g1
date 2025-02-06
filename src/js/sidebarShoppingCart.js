7ee4db52b18aa3ac6ff7c29932b74d022881eeb7
Eduardo Lemus <eduardolemus3098@gmail.com>
2025-02-05 18:59:28 -0600

    carrito sticky aunque se scrolee no se va a mover

--- src/js/sidebarShoppingCart.js
+++ src/js/sidebarShoppingCart.js
@@ -6,11 +6,11 @@ const sidebarShoppingCartContainer = document.getElementById(
 // sidebar container
 
 shoppingCartNavBarBtn.addEventListener("click", (e) => {
-  sidebarShoppingCartContainer.classList.remove("translate-x-full");
   sidebarShoppingCartContainer.classList.add("translate-x-0");
+  sidebarShoppingCartContainer.classList.remove("translate-x-full");
 });
 
 shoppingCartCloseBtn.addEventListener("click", (e) => {
-  sidebarShoppingCartContainer.classList.remove("translate-x-0");
   sidebarShoppingCartContainer.classList.add("translate-x-full");
+  sidebarShoppingCartContainer.classList.remove("translate-x-0");
 });
